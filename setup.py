ab0e01016384d904483c79b8fb227a4e882a26da
Robert Bost <rbost@redhat.com>
2021-07-11 15:14:57 -0600

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+from setuptools import setup, find_packages
+
+setup(
+    name="prow-adapter",
+    version='0.0.1',
+    packages=find_packages(),
+    author="rvanderp3",
+    entry_points={
+        "console_scripts": ["prowler=prowler"]
+    },
+    description="Provide a quick method of extracting prow data and logs for analysis.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    license="",
+    url="https://github.com/rvanderp3/prow-adapter",
+    install_requires=[
+        "pyyaml",
+    ],
+)
